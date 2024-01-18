438d7342c5285719ff48cfdfd0dff8eb53eb6f3d
Erik Osterman <erik@cloudposse.com>
2024-01-18 07:04:23 -0600

    install noto color emoji from google

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,19 @@ set -ex
 # Install Additional Fonts
 mkdir -p ~/.fonts
 
-# Install Twitter Emoji Font
 # Solution found in https://stackoverflow.com/questions/50662388/running-headless-chrome-puppeteer-with-no-sandbox
-#curl --location --silent --show-error -o ~/.fonts/emojione-android.ttf  https://github.com/emojione/emojione-assets/releases/download/3.1.2/emojione-android.ttf
+
+# Install Noto Color Emoji
+curl --location --silent --show-error -o ~/.fonts/NotoColorEmoji.ttf https://github.com/googlefonts/noto-emoji/blob/main/fonts/NotoColorEmoji.ttf?raw=true
+
+# Install Twitter Emoji Font
 wget https://github.com/13rac1/twemoji-color-font/releases/download/v14.0.2/TwitterColorEmoji-SVGinOT-Linux-14.0.2.tar.gz
 tar -xzvf TwitterColorEmoji-*.tar.gz --wildcards '*.ttf'
 find . -type f -wholename 'TwitterColorEmoji-*/*.ttf' -exec mv {} ~/.fonts/ \;
 
 # Rebuild the font cache
 fc-cache -f -v
+fc-list | grep -i emoji
 
 # Install dependencies
 npm install --no-fund option 
