0fc2a94d6c21ba2c4714332b91e0a8e01490ac02
Erik Osterman <erik@cloudposse.com>
2024-01-18 06:20:27 -0600

    mkdir fonts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+mkdir -p ~/.fonts
 curl --location --silent --show-error -o ~/.fonts/emojione-android.ttf  https://github.com/emojione/emojione-assets/releases/download/3.1.2/emojione-android.ttf
 fc-cache -f -v
 npm install --no-fund option 
