190725731653581db59409d7dc236c81aa7b8376
Erik Osterman <erik@cloudposse.com>
2024-01-18 07:16:17 -0600

    pin versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,18 @@
 set -ex
 
 # Install Additional Fonts
+TWITTER_COLOR_EMOJI_VERSION="14.0.2"
+GOOGLE_NOTO_COLOR_EMOJI_VERSION="2.042"
+
 mkdir -p ~/.fonts
 
 # Solution found in https://stackoverflow.com/questions/50662388/running-headless-chrome-puppeteer-with-no-sandbox
 
 # Install Noto Color Emoji
-curl --location --silent --show-error -o ~/.fonts/NotoColorEmoji.ttf https://github.com/googlefonts/noto-emoji/blob/main/fonts/NotoColorEmoji.ttf?raw=true
+curl --location --silent --show-error -o ~/.fonts/NotoColorEmoji.ttf  https://github.com/googlefonts/noto-emoji/blob/v${GOOGLE_NOTO_COLOR_EMOJI_VERSION}/fonts/NotoColorEmoji.ttf?raw=true
 
 # Install Twitter Emoji Font
-wget https://github.com/13rac1/twemoji-color-font/releases/download/v14.0.2/TwitterColorEmoji-SVGinOT-Linux-14.0.2.tar.gz
+wget https://github.com/13rac1/twemoji-color-font/releases/download/v${TWITTER_COLOR_EMOJI_VERSION}/TwitterColorEmoji-SVGinOT-Linux-${TWITTER_COLOR_EMOJI_VERSION}.tar.gz
 tar -xzvf TwitterColorEmoji-*.tar.gz --wildcards '*.ttf'
 find . -type f -wholename 'TwitterColorEmoji-*/*.ttf' -exec mv {} ~/.fonts/ \;
 
