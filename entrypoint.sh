55bb47c395377b07b388163585514b6be156acd6
Erik Osterman <erik@cloudposse.com>
2024-01-18 06:55:05 -0600

    use Twitter Emoji Font

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -ex
+
+# Install Additional Fonts
 mkdir -p ~/.fonts
-curl --location --silent --show-error -o ~/.fonts/emojione-android.ttf  https://github.com/emojione/emojione-assets/releases/download/3.1.2/emojione-android.ttf
+
+# Install Twitter Emoji Font
+# Solution found in https://stackoverflow.com/questions/50662388/running-headless-chrome-puppeteer-with-no-sandbox
+#curl --location --silent --show-error -o ~/.fonts/emojione-android.ttf  https://github.com/emojione/emojione-assets/releases/download/3.1.2/emojione-android.ttf
+wget https://github.com/13rac1/twemoji-color-font/releases/download/v14.0.2/TwitterColorEmoji-SVGinOT-Linux-14.0.2.tar.gz
+tar -xzvf TwitterColorEmoji-*.tar.gz --wildcards '*.ttf'
+find . -type f -wholename 'TwitterColorEmoji-*/*.ttf' -exec mv {} ~/.fonts/ \;
+
+# Rebuild the font cache
 fc-cache -f -v
+
+# Install dependencies
 npm install --no-fund option 
+
+# Generate screenshot
 node main.js
